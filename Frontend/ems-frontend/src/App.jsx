24fcc12f43e4ac36171691d43f8167f1b2a04858
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2023-12-27 13:13:01 +0530

    complete route component

--- Frontend/ems-frontend/src/App.jsx
+++ Frontend/ems-frontend/src/App.jsx
@@ -2,15 +2,21 @@ import './App.css'
 import FotterComponent from './components/FotterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeCompnent from './components/ListEmployeeCompnent'
+import {BrowserRouter,Route, Routes} from 'react-router-dom'
 
 function App() {
  
 
   return (
     <>
+    <BrowserRouter>
     <HeaderComponent/>
-     <ListEmployeeCompnent/>
+    <Routes>
+    <Route path="/" element={<ListEmployeeCompnent />} exact />
+    <Route path="/employees" element={<ListEmployeeCompnent />} exact />
+     </Routes>
      <FotterComponent/>
+     </BrowserRouter>
     </>
   )
 }
